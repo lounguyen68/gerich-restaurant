525b66b7d1d915f2c191c6a494ab7e1749c190fe
lounguyen68 <lou9.works@gmail.com>
2023-02-07 12:07:11 +0700

    fixed

--- src/container/Findus/FindUs.jsx
+++ src/container/Findus/FindUs.jsx
@@ -8,7 +8,7 @@ const FindUs = () => (
   <div className="app__bg app__wrapper section__padding" id="contact">
     <div className="app__wrapper_info">
       <SubHeading title="Contact"/>
-      <h1 className="headtext_cormorant" style={{marginBottom: '3rem'}}>Find Us</h1>
+      <h1 className="headtext__cormorant" style={{marginBottom: '3rem'}}>Find Us</h1>
 
       <div className="app__wrapper-content">
         <p className="p__opensans">Lane Ends Bungalow, Whatcroft Hall Lane, Rudheath, CW9 7SG</p>
